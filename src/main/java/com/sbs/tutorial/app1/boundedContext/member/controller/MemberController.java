d91ce769ebbacef5c30b006997d56e6ae988bbec
SangWon7242 <gotkdqja@gmail.com>
2025-01-26 03:31:36 +0900

    세션을 직접 다루지 않고 스프링시큐리티 이용

--- src/main/java/com/sbs/tutorial/app1/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/sbs/tutorial/app1/boundedContext/member/controller/MemberController.java
@@ -2,8 +2,12 @@ package com.sbs.tutorial.app1.boundedContext.member.controller;
 
 import com.sbs.tutorial.app1.boundedContext.member.entity.Member;
 import com.sbs.tutorial.app1.boundedContext.member.service.MemberService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +16,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.security.Principal;
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
 
   private final MemberService memberService;
-
+  private final PasswordEncoder passwordEncoder;
 
   @GetMapping("/join")
   public String showJoin() {
@@ -26,32 +32,31 @@ public class MemberController {
   }
 
   @PostMapping("/join")
-  public String join(String username, String password, String email, MultipartFile profileImg, HttpSession session) {
+  public String join(HttpServletRequest req, String username, String password, String email, MultipartFile profileImg) {
     Member oldMember = memberService.getMemberByUsername(username);
+
+    String passwordClearText = password;
+    password = passwordEncoder.encode(password);
 
     if (oldMember != null) {
       return "redirect:/?errorMsg=Already done.";
     }
 
-    Member member = memberService.join(username, "{noop}" + password, email, profileImg);
+    Member member = memberService.join(username, password, email, profileImg);
 
-
-    session.setAttribute("loginedMemberId", member.getId());
-
+    try {
+      req.login(username, passwordClearText); // 로그인 처리
+    } catch (ServletException e) {
+      throw new RuntimeException(e);
+    }
+    
     return "redirect:/member/profile";
   }
 
+  @PreAuthorize("isAuthenticated()")
   @GetMapping("/profile")
-  public String showProfile(HttpSession session, Model model) {
-    Long loginedMemberId = (Long) session.getAttribute("loginedMemberId");
-
-    boolean isLogined = loginedMemberId != null;
-
-    if (!isLogined) {
-      return "redirect:/?errorMsg=Need to login!";
-    }
-
-    Member loginedMember = memberService.getMemberById(loginedMemberId);
+  public String showProfile(Principal principal, Model model) {
+    Member loginedMember = memberService.getMemberByUsername(principal.getName());
 
     model.addAttribute("loginedMember", loginedMember);
 
