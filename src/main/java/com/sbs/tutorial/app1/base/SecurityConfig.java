8ab61015a787c2a4facca0dd5631662c6efa91dc
SangWon7242 <gotkdqja@gmail.com>
2025-08-12 08:50:45 +0900

    로그인 폼

--- src/main/java/com/sbs/tutorial/app1/base/SecurityConfig.java
+++ src/main/java/com/sbs/tutorial/app1/base/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
+    http // csrf.disable() : 완전 비활성화
         .csrf(csrf -> csrf.ignoringRequestMatchers("/**")) // 특정 경로는 CSRF 제외
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/", "/member/join").permitAll()
